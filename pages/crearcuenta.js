e027dc8a3881bba5ce20852b2c84fff5bdd6808a
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-10-22 11:19:31 -0300

    Formik ok in crearcuenta and login

--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -3,18 +3,21 @@ import Layout from "../components/Layout";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-const login = () => {
+const crearcuenta = () => {
   // Formulario y validación con formik y yup
   const formik = useFormik({
     initialValues: {
+      nombre: "",
       email: "",
       password: "",
     },
     validationSchema: Yup.object({
+      nombre: Yup.string().required("El nombre es obligatorio"),
       email: Yup.string()
         .email("El email no es válido")
         .required("El mail es obligatorio"),
       password: Yup.string()
+        .min(6, "El password debe contener al menos 6 caracteres")
         .required("El password es obligatorio"),
     }),
     onSubmit: (valores) => {
@@ -26,7 +29,7 @@ const login = () => {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
         <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">
-          Iniciar Sesión
+          Crear Cuenta
         </h2>
 
         <div className="flex justify-center mt-5">
@@ -35,6 +38,31 @@ const login = () => {
               className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
               onSubmit={formik.handleSubmit}
             >
+              <div className="mb-4">
+                <label
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="shadow appeareance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+
+              {formik.touched.nombre && formik.errors.nombre ? (
+                <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                  <p className="font-bold">Error</p>
+                  <p>{formik.errors.nombre}</p>
+                </div>
+              ) : null}
+
               <div className="mb-4">
                 <label
                   className="block text-black text-sm font-bold mb-2"
@@ -88,7 +116,7 @@ const login = () => {
               <input
                 type="submit"
                 className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white upercase font-bold cursor-pointer"
-                value="Iniciar sesión"
+                value="Crear cuenta"
               />
             </form>
           </div>
@@ -98,4 +126,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default crearcuenta;
